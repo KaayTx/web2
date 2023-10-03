979db6cad38f84d80012ccfdc96cc1abeacd7ef7
KaayTx <kawtar.dahman@student.vinci.be>
2023-10-03 02:01:53 +0100

    pizzeria

--- tutorials/pizzeria/api/basic/app.js
+++ tutorials/pizzeria/api/basic/app.js
@@ -3,7 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var pizzaRouter = require('./routes/pizzas');
+var indexRouter = require('./routes/index');
+var pizzaRouter = require('./routes/pizza');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -12,9 +13,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/', pizzaRouter);
+app.use('/', indexRouter);
+app.use('/pizzas', pizzaRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
