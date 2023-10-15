1097c9f635866b8c2d6aad7c83725d4fad7730d0
KaayTx <kawtar.dahman@student.vinci.be>
2023-10-15 04:03:05 +0100

    1.3 : API : create & REST client

--- exercices/1.3/app.js
+++ exercices/1.3/app.js
@@ -1,37 +1,40 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var filmRouter = require('./routes/films');
 
 var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-const stats = {};
+app.use('/', indexRouter);
+app.use('/films', filmRouter);
 
-app.use((req, res, next) => {
-  const currentOperation = `${req.method} ${req.path}`;
-  const currentOperationCounter = stats[currentOperation];
-  if (currentOperationCounter === undefined) stats[currentOperation] = 0;
-  stats[currentOperation] += 1;
-  const statsMessage = `Request counter : \n${Object.keys(stats)
-    .map((operation) => `- ${operation} : ${stats[operation]}`)
-    .join('\n')}
-      `;
-  console.log(statsMessage);
-  next();
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
