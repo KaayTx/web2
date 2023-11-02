656c59ba9c3afbc7d36f7c47e6932ad3828585e3
KaayTx <kawtar.dahman@student.vinci.be>
2023-11-02 23:11:01 +0100

    2.4.2 et projet

---
+++ exercices/2.4.2/index.js
@@ -0,0 +1,12 @@
+const redLamp = document.querySelector('.red');
+const orangeLamp = document.querySelector('.orange');
+const greenLamp = document.querySelector('.green');
+
+const redColor = 'Red';
+const orangeColor = 'Orange';
+const greenColor = 'Green';
+const noColor = '';
+
+
+
+
---
+++ project/README.md
@@ -0,0 +1,6 @@
+La grande critique
+
+Description :
+
+
+
