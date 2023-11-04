83198bfef5a278e304c695b834034fb3f405178f
KaayTx <kawtar.dahman@student.vinci.be>
2023-11-04 17:53:33 +0100

    1.8 : API : linter & formatter

--- exercices/1.8/routes/films.js
+++ exercices/1.8/routes/films.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const path = require('node:path');
 const { serialize, parse } = require('../utils/json');
 
 const router = express.Router();
 
-// eslint-disable-next-line import/order
-const path = require('node:path');
 
 const jsonDbPath = path.join(__dirname, '/../data/films.json');
 
@@ -34,30 +33,32 @@ const MOVIES = [
 
 // Filtered by minimum-duration
 router.get('/', (req, res) => {
-  const minDuration = req?.query?.['minimum-duration']
-    ? Number(req.query['minimum-duration'])
-    : undefined;
+  // Avant y'avait ça et ça marchait pas
+ // const minDuration = req?.query?.['minimum-duration']
+//  ? Number(req.query['minimum-duration']) : undefined;
+// Faut mettre comme ça pour dire -> Read all the films, filtered by minimum-duration if the query param exists
+const minDuration = req?.query ? Number(req.query['minimum-duration'])
+  : undefined;
 
   if (typeof minDuration !== 'number' || minDuration <= 0) return res.sendStatus(400);
 
-  const movies = parse(jsonDbPath, MOVIES);
+  const films = parse(jsonDbPath, MOVIES);
 
-  if (!minDuration) return res.json(MOVIES); // si on trouve pas, alors on renvoie tous les films qu'on a
+  if (!minDuration) return res.json(films); // si on trouve pas, alors on renvoie tous les films qu'on a
 
-  const filmsReachingMinDuration = movies.filter((film) => film.duration >= minDuration);
+  const filmsReachingMinDuration = films.filter((film) => film.duration >= minDuration);
 
   return res.json(filmsReachingMinDuration);
 });
 
 // Get id
 router.get('/:id', (req, res) => {
-  const movies = parse(jsonDbPath, MOVIES);
+  const films = parse(jsonDbPath, MOVIES);
 
-  // eslint-disable-next-line eqeqeq
-  const movieId = MOVIES.findIndex((film) => film.id == req.params.id);
+  const movieId = films.findIndex((film) => film.id === Number(req.params.id));
   if (movieId < 0) return res.sendStatus(404);
 
-  return res.json(movies[movieId]);
+  return res.json(films[movieId]);
 });
 
 // Create film
@@ -74,24 +75,22 @@ router.post('/', (req, res) => {
   const budget =
     typeof req?.body?.budget !== 'number' || req.body.budget < 0 ? undefined : req.body.budget;
 
-  if (!title || !link || !duration || !budget) return res.sendStatus(409);
+  if (!title || !link || !duration || !budget) return res.sendStatus(400);
 
-  const movies = parse(jsonDbPath, MOVIES);
+  const films = parse(jsonDbPath, MOVIES);
 
-  const existingMovie = movies.find((film) => film.title.toLowerCase() === title.toLowerCase());
+  const existingMovie = films.find((film) => film.title.toLowerCase() === title.toLowerCase());
 
   if (existingMovie) return res.sendStatus(409); // existe déjà
 
-  const lastIdIndex = movies?.length !== 0 ? movies.length - 1 : undefined;
-  const lastId = lastIdIndex !== undefined ? movies[lastIdIndex]?.id : 0;
+  const lastIdIndex = films?.length !== 0 ? films.length - 1 : undefined;
+  const lastId = lastIdIndex !== undefined ? films[lastIdIndex]?.id : 0;
   const nextId = lastId + 1;
-  console.log(nextId);
 
   const newFilm = { id: nextId, title, link, duration, budget };
-  console.log(newFilm);
 
-  movies.push(newFilm);
-  serialize(jsonDbPath, movies);
+  films.push(newFilm);
+  serialize(jsonDbPath, films);
 
   return res.json(newFilm);
 });
@@ -100,11 +99,10 @@ router.post('/', (req, res) => {
 router.delete('/:id', (req, res) => {
   const films = parse(jsonDbPath, MOVIES);
 
-  // eslint-disable-next-line no-use-before-define, eqeqeq
-  const movieId = movieRemoved.findIndex((film) => film.id == req.params.id);
+  const movieId = films.findIndex((film) => film.id === Number(req.params.id));
   if (movieId < 0) return res.sendStatus(404);
 
-  const itemsRemoved = MOVIES.splice(movieId, 1);
+  const itemsRemoved = films.splice(movieId, 1);
   const movieRemoved = itemsRemoved[0];
 
   serialize(jsonDbPath, films);
@@ -131,7 +129,7 @@ router.patch('/:id', (req, res) => {
   const films = parse(jsonDbPath, MOVIES);
 
   // eslint-disable-next-line eqeqeq
-  const movieId = films.findIndex((film) => film.id == req.params.id);
+  const movieId = films.findIndex((film) => film.id === Number(req.params.id));
   if (movieId < 0) return res.sendStatus(404);
 
   const movieUpdated = { ...films[movieId], ...req.body };
@@ -164,7 +162,7 @@ router.put('/:id', (req, res) => {
   )
     return res.sendStatus(400);
 
-  const {id} = req.params;
+  const id = Number(req.params.id);
   const films = parse(jsonDbPath, MOVIES);
 
   const movieId = films.findIndex((film) => film.id === id);
