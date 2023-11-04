4c1b9faee477b965e9348b29f70d5f1df0ae0a5f
KaayTx <kawtar.dahman@student.vinci.be>
2023-11-04 23:35:13 +0100

    2.4.2 : timed events

--- exercices/2.4.2/index.js
+++ exercices/2.4.2/index.js
@@ -1,12 +1,76 @@
-const redLamp = document.querySelector('.red');
-const orangeLamp = document.querySelector('.orange');
-const greenLamp = document.querySelector('.green');
+const redLamp = document.querySelector(".red");
+const orangeLamp = document.querySelector(".orange");
+const greenLamp = document.querySelector(".green");
 
-const redColor = 'Red';
-const orangeColor = 'Orange';
-const greenColor = 'Green';
-const noColor = '';
+const redColor = "red";
+const orangeColor = "orange";
+const greenColor = "green";
+const noColor = "";
+
+const delayBetweenChanges = 1000;
+
+cycleThroughLamps();
+
+function cycleThroughLamps() {
+  window.setInterval(
+    showFromRedToGreenToRedWithDelays,
+    delayBetweenChanges * 4
+  );
+}
+
+showFromRedToGreenToRedWithDelays();
+
+function showFromRedToGreenToRedWithDelays() {
+  showRedLamp();
+  showOrangeLampOnlyWithDelay(delayBetweenChanges);
+  showGreenLampOnlyWithDelay(2*delayBetweenChanges);
+  showOrangeLampOnlyWithDelay(3*delayBetweenChanges);
+  showRedLampOnlyWithDelay(4*delayBetweenChanges);
+}
+
+function showRedLamp() {
+  redLamp.style.backgroundColor = redColor;
+}
+function clearRedLamp() {
+  redLamp.style.backgroundColor = noColor;
+}
+
+function showRedLampOnlyWithDelay(delay) {
+    window.setTimeout(() => {
+        clearOrangeLamp();
+        clearGreenLamp();
+        showRedLamp();
+    }, delay);
+}
 
 
+function showOrangeLamp() {
+  orangeLamp.style.backgroundColor = orangeColor;
+}
 
+function clearOrangeLamp() {
+  orangeLamp.style.backgroundColor = noColor;
+}
 
+function showOrangeLampOnlyWithDelay(delay) {
+  window.setTimeout(() => {
+    clearRedLamp();
+    clearGreenLamp();
+    showOrangeLamp();
+  }, delay);
+}
+
+function showGreenLampOnlyWithDelay(delay) {
+  window.setTimeout(() => {
+    clearRedLamp();
+    clearOrangeLamp();
+    showGreenColor();
+  }, delay);
+}
+
+function showGreenColor() {
+  greenLamp.style.backgroundColor = greenColor;
+}
+function clearGreenLamp() {
+  greenLamp.style.backgroundColor = noColor;
+}
