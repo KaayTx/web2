2ebd5e66c63dbfa346ebea04146099314b0f793f
KaayTx <kawtar.dahman@student.vinci.be>
2023-11-13 14:32:33 +0100

    2.9: dynamic page content

--- project/2.7/src/index.js
+++ project/2.7/src/index.js
@@ -4,7 +4,16 @@ import './stylesheets/main.css';
 import grootImage from './img/groot.jpg';
 import stormtrooperImage from './img/stormtrooper.jpg';
 
-const homePage =  `
+// le const main faut le faire avant de faire appel à la fonction renderHomePage 
+// sinon ça marche pas et on n'a pas le main qui s'affiche sur la page
+const main = document.querySelector('main');
+
+
+renderHomePage();
+
+
+function renderHomePage() {
+  const homePage = `
 <div class="container text-center">
         <div class="row">
           <div class="col">
@@ -13,6 +22,12 @@ const homePage =  `
             <p>Here you can find a selection of our favorite movies ; )</p>
           </div>
         </div>
+
+        <div class="row mb-3">
+        <div class="col">
+          <button type="button" class="btn btn-dark">About</button>
+        </div>
+
 
         <div class="row">
           <div class="col-12 col-lg-6">
@@ -25,5 +40,29 @@ const homePage =  `
         </div>
 </div>`;
 
-const main = document.querySelector('main');
-main.innerHTML = homePage;
+  main.innerHTML = homePage;
+  const button = document.querySelector('button');
+  button.addEventListener('click', renderAboutPage);
+}
+
+function renderAboutPage(){
+  const aboutPage = `
+  <div class="container text-center">
+          <div class="row">
+            <div class="col">
+              <h3>About e-baron...</h3>
+
+              <p>He is a teacher at Vinci who enjoys coding in JS...</p>
+            </div>
+          </div>  
+          <div class="row mb-3">
+            <div class="col">
+              <button type="button" class="btn btn-dark">Back</button>
+            </div>
+          </div>        
+  </div>`;
+
+  main.innerHTML = aboutPage;
+  const button = document.querySelector('button');
+  button.addEventListener('click', renderHomePage);
+}
